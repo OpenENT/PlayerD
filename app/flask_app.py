0218777ac7913431abcbe978bc95993f7d27307d
Mast03 <antoniobased@icloud.com>
2021-08-20 22:50:04 +0200

    Initial flask app

---
+++ app/flask_app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from player import MPVPlayer
+import json
+
+app = Flask(__name__)
+mp = MPVPlayer()
+
+@app.route('/')
+def root():
+    return 'MPVd working'
+
+@app.route('/status')
+def status():
+    return mp.get_status()
+
+@app.route('/action')
+def action():
+
+    action = request.args.get('type')
+    arg = request.args.get('arg')
+
+    if action == "resume":
+        mp.resume()
+    elif action == "pause":
+        mp.pause()
+    elif action == "close":
+        mp.close()
+    elif action == "go_at":
+        if arg == None:
+            return {'status': 400}
+        mp.go_at(arg)
+    elif action == "play":
+        if arg == None:
+            return {'status': 400}
+        mp.play(arg)
+    
+    return {'status': 200}
