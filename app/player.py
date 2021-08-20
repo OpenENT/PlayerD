c78fdbcdb15a8386324b7189d5e3262671ca96dd
Mast03 <antoniobased@icloud.com>
2021-08-20 22:49:57 +0200

    Player interface

---
+++ app/player.py
@@ -0,0 +1,27 @@
+class Player:
+
+    def __init__(self):
+        pass
+
+    def play(url: str):
+        pass
+
+    def resume():
+        pass
+
+    def pause():
+        pass
+
+    def close():
+        pass
+
+    def go_at(time):
+        pass
+    
+    def get_status(): # {"paused": false, "duration": "00:01:00", "at": "00:00:30", "name": "song", "url": "url"}
+        pass
+
+
+class MPVPlayer(Player):
+
+    pass
