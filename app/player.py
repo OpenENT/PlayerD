4064d11c0c9379662cae4cf5998c12dbd29174a8
Mast03 <antoniobased@icloud.com>
2021-08-21 11:16:16 +0200

    Initial MPV implementation

--- app/player.py
+++ app/player.py
@@ -1,27 +1,52 @@
+from mpv import MPV
+
 class Player:
 
     def __init__(self):
         pass
 
-    def play(url: str):
+    def play(self, url: str):
         pass
 
-    def resume():
+    def resume(self):
         pass
 
-    def pause():
+    def pause(self):
         pass
 
-    def close():
+    def close(self):
         pass
 
-    def go_at(time):
+    def go_at(self, seconds: int):
         pass
     
-    def get_status(): # {"paused": false, "duration": "00:01:00", "at": "00:00:30", "name": "song", "url": "url"}
+    def get_status(self): # {"paused": false, "duration": "60", "position": "30", "name": "song", "url": "url"}
         pass
 
 
 class MPVPlayer(Player):
 
-    pass
+    def __init__(self):
+        self.player = MPV(video=False)
+        
+    def play(self, url: str):
+        self.player.play(url)
+    
+    def resume(self):
+        self.player.pause = False
+
+    def pause(self):
+        self.player.pause = True
+
+    def close(self):
+        self.player.stop()
+
+    def go_at(self, seconds: int):
+        self.player.time_pos = seconds
+
+    def get_status(self):
+        if self.player.time_pos is None:
+            return {"playing": False}
+        return {"playing": True, "paused": self.player.pause, "duration": self.player.duration, "position": self.player.time_pos, "name": self.player.media_title, "url": self.player.filename}
+
+    
